64e274eb5944d9eb0f4568aa9b6f2ec8ff124181
Niamh <niamh.gowran2@mail.dcu.ie>
2022-10-22 17:26:22 +0100

    Background & project component

---
+++ components/projects.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import { useState } from "react";
+import Swipe from "react-easy-swipe";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
+
+/**
+ * Carousel component for nextJS and Tailwind.
+ * Using external library react-easy-swipe for swipe gestures on mobile devices (optional)
+ *
+ * @param images - Array of images with src and alt attributes
+ * @returns React component
+ */
+export default function Project_Carousel() {
+  
+
+  return (
+    <div>
+        
+    </div>
+  );
+}
