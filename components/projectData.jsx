745a414a005a2314bc6bcb03baae044f8d103ece
Niamh <niamh.gowran2@mail.dcu.ie>
2022-10-22 21:04:15 +0100

    Projects

---
+++ components/projectData.jsx
@@ -0,0 +1,113 @@
+import Image from 'next/image'
+import logo from '../public/logo.png'
+
+export const CarouselData = [
+    {
+      "card":
+            <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                <a href="http://vocalise.ie/" target="blank">
+                    <Image 
+                    className="rounded-t-lg" 
+                    src={logo} 
+                    alt="Vocalise Logo"
+                    />
+                </a>
+                <div className="p-5">
+                    <a href="http://vocalise.ie/" target="blank">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Vocalise</h5>
+                    </a>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Info on Vocalise</p>
+                </div>
+            </div>
+    },
+    {
+        "card":
+              <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                  <a href="http://vocalise.ie/" target="blank">
+                      <Image 
+                      className="rounded-t-lg" 
+                      src={logo} 
+                      alt="Vocalise Logo"
+                      />
+                  </a>
+                  <div className="p-5">
+                      <a href="http://vocalise.ie/" target="blank">
+                          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Vocalise</h5>
+                      </a>
+                      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Info on Vocalise</p>
+                  </div>
+              </div>
+      },
+      {
+        "card":
+              <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                  <a href="http://vocalise.ie/" target="blank">
+                      <Image 
+                      className="rounded-t-lg" 
+                      src={logo} 
+                      alt="Vocalise Logo"
+                      />
+                  </a>
+                  <div className="p-5">
+                      <a href="http://vocalise.ie/" target="blank">
+                          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Vocalise</h5>
+                      </a>
+                      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Info on Vocalise</p>
+                  </div>
+              </div>
+      },
+      {
+        "card":
+              <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                  <a href="http://vocalise.ie/" target="blank">
+                      <Image 
+                      className="rounded-t-lg" 
+                      src={logo} 
+                      alt="Vocalise Logo"
+                      />
+                  </a>
+                  <div className="p-5">
+                      <a href="http://vocalise.ie/" target="blank">
+                          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Vocalise</h5>
+                      </a>
+                      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Info on Vocalise</p>
+                  </div>
+              </div>
+      },
+      {
+        "card":
+              <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                  <a href="http://vocalise.ie/" target="blank">
+                      <Image 
+                      className="rounded-t-lg" 
+                      src={logo} 
+                      alt="Vocalise Logo"
+                      />
+                  </a>
+                  <div className="p-5">
+                      <a href="http://vocalise.ie/" target="blank">
+                          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Vocalise</h5>
+                      </a>
+                      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Info on Vocalise</p>
+                  </div>
+              </div>
+      },
+    {
+        "card":
+              <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                  <a href="http://vocalise.ie/" target="blank">
+                      <Image 
+                      className="rounded-t-lg" 
+                      src={logo} 
+                      alt="ReNu Logo"
+                      />
+                  </a>
+                  <div className="p-5">
+                      <a href="http://vocalise.ie/" target="blank">
+                          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Renu</h5>
+                      </a>
+                      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Info on ReNu</p>
+                  </div>
+              </div>
+      }
+]
